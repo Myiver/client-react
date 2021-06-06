cedfc628f651dc71ed64ddfe40267ba1d9a3ca4a
Ararat1 <matinyan_0@mail.ru>
2021-06-06 22:20:36 +0400

    Depoloyed to GitHub Pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import { BrowserRouter as Router } from "react-router-dom"
+import { HashRouter as Router } from "react-router-dom"
+// import { BrowserRouter as Router } from "react-router-dom"
 import { Provider } from "react-redux"
 import { ThemeProvider } from "@material-ui/core"
 
