fe2bcd3933689a10e0e6d1488cccecdb43e385d6
Ararat1 <matinyan_0@mail.ru>
2021-06-10 11:56:22 +0400

    Changed Database Link

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,2 +1,2 @@
-export const DB_Link = "http://localhost:8080/api"
-// export const DB_Link = "https://myiver.herokuapp.com/api"
+// export const DB_Link = "http://localhost:8080/api"
+export const DB_Link = "https://myiver.herokuapp.com/api"
